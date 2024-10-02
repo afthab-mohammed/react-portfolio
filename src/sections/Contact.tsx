a45b5965be9e7f2e69b65c6be1ed19f8539aa0d2
afthab-mohammed <afthabmohammed45@gmail.com>
2024-10-02 18:10:49 +0530

    first commit

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,19 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+
+export const ContactSection = () => {
+  return (
+  <div className="py-16 pt-14">
+    <div className="container ">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+    <h2 className="font-serif text-2xl">Let's create something Amazing Together!</h2>
+    <p className="text-sm mt-2">Ready to bring your next project to life? Let's connect and discuss. I can help you achieve your goals. </p>
+    <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 margin-top mt-8">
+      <span className="font-semibold">Contact Me</span>
+      <ArrowUpRightIcon className='size-4' />
+      </button>
+  </div>
+  </div>
+  </div>
+  );
+};
